342c2e1124057f9c35e42988abaa15c95b667d2f
Pablo Vieira <pablomgvieira@gmail.com>
2018-10-02 22:55:41 -0300

    - Facade Done

--- src/pratica/banco/negocio/Fachada.java
+++ src/pratica/banco/negocio/Fachada.java
@@ -42,43 +42,37 @@ public class Fachada {
         clientes.insert(cliente);
     }
 
-    public Cliente removerCliente(Cliente cliente) throws ClienteInexistenteException, ClienteContaCadastradaException, ArvoreVaziaException {
-        ArrayList<ContaAbstrata> elements = new ArrayList<>();
-        elements = inorderIterate(elements);
-        if (elements == null) {
-            return clientes.remove(cliente);
-        }
-        ArrayList<ContaAbstrata> accounts = new ArrayList<>();
-        for (ContaAbstrata element : elements) {
-            if (cliente.getCpf().equals(element.getCliente().getCpf())) {
-                accounts.add(element);
-            }
-        }
-        if (accounts.size() == 0) {
-            return clientes.remove(cliente);
-        } else {
-            throw new ClienteContaCadastradaException(cliente.getCpf());
-        }
-
-    }
-
-    public Cliente removerClienteRaiz() throws ClienteInexistenteException, ArvoreVaziaException {
-        return clientes.removeRoot();
-    }
-    
-    public void exibirClientes() throws ArvoreVaziaException {
-        ArrayList<Cliente> listaClientes = clientes.inorderIterate();
-        for (Cliente cliente : listaClientes) {
-            System.out.println(cliente);
-        }
+    public Cliente procurarCliente(String cpf) throws ClienteInexistenteException, ArvoreVaziaException {
+        return clientes.search(cpf);
     }
 
     public Cliente atualizarCliente(Cliente cliente) throws ClienteInexistenteException, ArvoreVaziaException {
         return clientes.update(cliente);
     }
 
-    public Cliente procurarCliente(String cpf) throws ClienteInexistenteException, ArvoreVaziaException {
-        return clientes.search(cpf);
+    public Cliente removerCliente(Cliente cliente) throws ClienteInexistenteException, ClienteContaCadastradaException, ArvoreVaziaException {
+        ArrayList<ContaAbstrata> accounts = contas.inorderIterate();
+        if (accounts == null) {
+            return clientes.remove(cliente);
+        } else {
+            for (ContaAbstrata account : accounts) {
+                if (cliente.getCpf().equals(account.getCliente().getCpf())) {
+                    throw new ClienteContaCadastradaException(cliente.getCpf());
+                }
+            }
+        }
+        return clientes.remove(cliente);
+    }
+
+    public Cliente removerClienteRaiz() throws ClienteInexistenteException, ArvoreVaziaException {
+        return clientes.removeRoot();
+    }
+
+    public void exibirClientes() throws ArvoreVaziaException {
+        ArrayList<Cliente> listaClientes = clientes.inorderIterate();
+        for (Cliente cliente : listaClientes) {
+            System.out.println(cliente);
+        }
     }
 
     public void cadastrarConta(ContaAbstrata conta) throws ContaExistenteException, ClienteInexistenteException, ClienteInvalidoException, ArvoreVaziaException {
@@ -91,35 +85,51 @@ public class Fachada {
         }
     }
 
-    public void removerConta(ContaAbstrata conta) throws ContaInexistenteException {
-        contas.remove(conta);
-    }
-
-    public ContaAbstrata removerContaRaiz() throws ContaInexistenteException {
-        return contas.removeRoot();
-    }
-
-    public void atualizarConta(ContaAbstrata conta) throws ContaInexistenteException {
-        contas.update(conta);
-    }
-
-    public ContaAbstrata procurarConta(String numero) throws ContaInexistenteException {
+    public ContaAbstrata procurarConta(String numero) throws ContaInexistenteException, ArvoreVaziaException {
         return contas.search(numero);
     }
 
-    public ArrayList<ContaAbstrata> inorderIterate(ArrayList<ContaAbstrata> list) {
-        return contas.inorderIterate(list);
+    public void atualizarConta(ContaAbstrata conta) throws ContaInexistenteException, ArvoreVaziaException {
+        contas.update(conta);
     }
 
-    public void creditar(String numero, double valor) throws ContaInexistenteException {
+    public void removerConta(ContaAbstrata conta) throws ContaInexistenteException, ArvoreVaziaException {
+        contas.remove(conta);
+    }
+
+    public ContaAbstrata removerContaRaiz() throws ContaInexistenteException, ArvoreVaziaException {
+        return contas.removeRoot();
+    }
+
+    public void exibirContas() throws ArvoreVaziaException {
+        ArrayList<ContaAbstrata> listaContas = contas.inorderIterate();
+        for (ContaAbstrata conta : listaContas) {
+            System.out.println(conta);
+        }
+    }
+    
+    public void exibirContasDoCliente(String cpf) throws ArvoreVaziaException {
+        ArrayList<ContaAbstrata> listaContas = contas.inorderIterate();
+        ArrayList<ContaAbstrata> contasDoCliente = new ArrayList<>();
+        for (ContaAbstrata conta : listaContas) {
+            if (cpf.equals(conta.getCliente().getCpf())) {
+                contasDoCliente.add(conta);
+            }
+        }
+        for (ContaAbstrata contaCliente : contasDoCliente) {
+            System.out.println(contaCliente);
+        }
+    }
+
+    public void creditar(String numero, double valor) throws ContaInexistenteException, ArvoreVaziaException {
         contas.creditar(numero, valor);
     }
 
-    public void debitar(String numero, double valor) throws ContaInexistenteException, SaldoInsuficienteException {
+    public void debitar(String numero, double valor) throws ContaInexistenteException, SaldoInsuficienteException, ArvoreVaziaException {
         contas.debitar(numero, valor);
     }
 
-    public void transferir(String numeroOrigem, String numeroDestino, double valor) throws ContaInexistenteException, SaldoInsuficienteException {
+    public void transferir(String numeroOrigem, String numeroDestino, double valor) throws ContaInexistenteException, SaldoInsuficienteException, ArvoreVaziaException {
         contas.transferir(numeroOrigem, numeroDestino, valor);
     }
 }
